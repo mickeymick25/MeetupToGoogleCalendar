0a72228f96e9f432af59cd0f75e9969c1188cf92
Michael Boitin <mboitin@octo.com>
2018-07-10 16:47:31 +0200

    Add mocha and run audit to correct vulnerabilities

--- index.js
+++ index.js
@@ -3,23 +3,23 @@ const Meetup = require('./meetup');
 const Google = require('./google');
 const isProd = false;
 
-function setupHttp(){
-    // Load HTTP module
-    let http = require("http");
-
-    // Create HTTP server and listen on port 8000 for requests
-    http.createServer(function(request, response) {
-
-       // Set the response HTTP header with HTTP status and Content type
-       response.writeHead(200, {'Content-Type': 'text/plain'});
-
-       // Send the response body "Hello World"
-       response.end('Hello World\n');
-    }).listen(8000);
-
-    // Print URL for accessing server
-    console.log('Server running at http://127.0.0.1:8000/');
-}
+// function setupHttp(){
+//     // Load HTTP module
+//     let http = require("http");
+//
+//     // Create HTTP server and listen on port 8000 for requests
+//     http.createServer(function(request, response) {
+//
+//        // Set the response HTTP header with HTTP status and Content type
+//        response.writeHead(200, {'Content-Type': 'text/plain'});
+//
+//        // Send the response body "Hello World"
+//        response.end('Hello World\n');
+//     }).listen(8000);
+//
+//     // Print URL for accessing server
+//     console.log('Server running at http://127.0.0.1:8000/');
+// }
 
 /**
  * Init the application by creating an http server, calling the meetup API,
